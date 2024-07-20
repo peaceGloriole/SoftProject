6caace446c2ee70fa37b812774c77a1dbc2ec7bf
peaceGloriole <dmtrpwlw@icloud.com>
2024-07-20 21:55:49 +0300

    register implemented

---
+++ src/components/register/Register.jsx
@@ -0,0 +1,43 @@
+import { Link } from "react-router-dom";
+
+export default function Register() {
+    return (
+        <section id="registerPage">
+            <form>
+                <fieldset>
+                    <legend>Register</legend>
+
+                    <label htmlFor="email" className="vhide">Email</label>
+                    <input
+                        id="email"
+                        className="email"
+                        name="email"
+                        type="text"
+                        placeholder="Email" />
+
+                    <label htmlFor="password" className="vhide">Password</label>
+                    <input
+                        id="password"
+                        className="password"
+                        name="password"
+                        type="password"
+                        placeholder="Password" />
+
+                    <label htmlFor="conf-pass" className="vhide">Confirm Password:</label>
+                    <input
+                        id="conf-pass"
+                        className="conf-pass"
+                        name="conf-pass"
+                        type="password"
+                        placeholder="Confirm Password" />
+
+                    <button type="submit" className="register">Register</button>
+
+                    <p className="field">
+                        <span>If you already have profile click <Link to="/login">here</Link></span>
+                    </p>
+                </fieldset>
+            </form>
+        </section>
+    );
+}
