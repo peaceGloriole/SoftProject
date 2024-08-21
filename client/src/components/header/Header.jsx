3c62193533f945167b898ee918a2a47ec4bd351a
peaceGloriole <dmtrpwlw@icloud.com>
2024-08-21 17:50:41 +0300

    user/guest views

--- client/src/components/header/Header.jsx
+++ client/src/components/header/Header.jsx
@@ -1,19 +1,30 @@
+import { useContext } from 'react';
 import { Link } from 'react-router-dom';
+import { AuthContext } from '../../contexts/AuthContext';
+
 
 export default function Header() {
+    const { isAuthenticated } = useContext(AuthContext);
     return (
         <header>
             <nav>
                 <img src="./images/headphones.png" />
-                    <Link to="/">Home</Link>
-                    <ul>
-                        <li><Link to="/albums">Catalog</Link></li>
-                        <li><Link to="/search">Search</Link></li>
-                        <li><Link to="/login">Login</Link></li>
-                        <li><Link to="/register">Register</Link></li>
-                        <li><Link to="/albums/create">Create Album</Link></li>
-                        <li><Link to="/logout">Logout</Link></li>
-                    </ul>
+                <Link to="/">Home</Link>
+                <ul>
+                    {isAuthenticated ? (
+                        <>
+                            <li><Link to="/albums">Catalog</Link></li>
+                            <li><Link to="/search">Search</Link></li>
+                            <li><Link to="/albums/create">Create Album</Link></li>
+                            <li><Link to="/logout">Logout</Link></li>
+                        </>
+                    ) : (
+                        <>
+                            <li><Link to="/login">Login</Link></li>
+                            <li><Link to="/register">Register</Link></li>
+                        </>
+                    )}
+                </ul>
             </nav>
         </header>
     );
