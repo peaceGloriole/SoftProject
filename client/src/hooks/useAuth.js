10559d9a9fef3c8134ce5c27ac5659070e8bb52e
peaceGloriole <dmtrpwlw@icloud.com>
2024-08-21 17:21:51 +0300

    global auth state

--- client/src/hooks/useAuth.js
+++ client/src/hooks/useAuth.js
@@ -1,10 +1,14 @@
+import { useContext } from "react";
 import { login } from "../api/auth-api";
+import { AuthContext } from "../contexts/AuthContext";
 
 export const useLogin = () => {
+    const { changeAuthState } = useContext(AuthContext);
+
     const loginHandler = async (email, password) => {
         const result = await login(email, password);
 
-        return result;
+        changeAuthState(result);
     };
 
     return loginHandler;
