0699ffff3d974690c323cd29d1551c4866395656
peaceGloriole <dmtrpwlw@icloud.com>
2024-08-21 21:49:17 +0300

    persisted state implemented, bug in _id to fix

--- client/src/contexts/AuthContext.jsx
+++ client/src/contexts/AuthContext.jsx
@@ -5,7 +5,7 @@ const AuthContext = createContext();
 
 export function AuthContextProvider(props) {
 
-    const [authState, setAuthState] = usePersistedState({});
+    const [authState, setAuthState] = usePersistedState(`auth`, {});
 
     const changeAuthState = (state) => {
         localStorage.setItem(`accessToken`, state.accessToken);
--- client/src/hooks/usePersistedState.js
+++ client/src/hooks/usePersistedState.js
@@ -1,7 +1,28 @@
 import { useState } from "react";
 
-export function usePersistedState(initialState) {
-    const [state, setState] = useState(initialState);
+export function usePersistedState(key, initialState) {
+    const [state, setState] = useState(() => {
+        const persistedState = localStorage.getItem(key);
 
-    return [state, setState];
+        if (persistedState) {
+            return typeof initialState === `function`
+                ? initialState()
+                : initialState;
+        }
+
+        const authData = JSON.parse(persistedState);
+        return authData;
+    });
+
+    const updateState = (value) => {
+        const newState = typeof value === `function`
+            ? value(state)
+            : value;
+
+        localStorage.setItem(key, JSON.stringify(newState));
+
+        setState(newState);
+    };
+
+    return [state, updateState];
 }
